d8330e45dedce1627946d5afa091a4a06ca047bd
SirOogway <jesusandresrodriguezgarcia@outlook.com>
2023-07-19 16:03:26 -0500

    Comments added

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
 <body>
     <?php
-
+    <!-- Ingreso la información de cada persona -->
     $header = array("Nombre", "Dirección", "Teléfono", "Color", "Significado");
     $jesus = array("Jesús", "Carrea 7B", "300214565", "Azul", "Cielo");
     $maria = array("Maria", "Carrea 8H", "321231567", "Amarillo", "Pollitos");
@@ -20,7 +20,7 @@
     $jose = array("José", "Carrea 2B", "465645656", "Verde", "Hulk");
     $alfonso = array("Alfonso", "Carrea 54F", "466546546", "Rosa", "Fresas");
     $alfonsa = array("Alfonsa", "Carrea 55G", "577657657", "Rosa", "Flor");
-
+    <!-- Asigno la información al array personas -->
     $personas = array($jesus, $maria, $carlos, $yamileth, $juana, $jose, $alfonso, $alfonsa);
 
     ?>
@@ -31,10 +31,12 @@
     <div class="container">
         <h2>Punto 1 y 2</h2>
         <!-- Start Table -->
+        <!-- le asigno los estilos con bootstrap -->
         <table class="table table-hover container">
             <!-- Start Table Head -->
             <thead>
                 <tr>
+                    <!-- Imprimo los datos de la cabecera -->
                     <?php
                 foreach ($header as $head) {
                     echo '<th scope="row">', $head, '</th>';
@@ -45,6 +47,7 @@
             <!-- End Table Head -->
             <!-- Start Table Body -->
             <tbody>
+                <!-- Aquí imprimo la información de la tabla en las filas -->
                 <tr>
                     <?php
                 for ($i = 0; $i < count($personas); $i++) {
@@ -89,6 +92,7 @@
                 $penultimate_index = array_key_last($personas[0]) - 1;
                 for ($i = 0; $i < count($personas); $i++) {
                     $persona = $personas[$i];
+                    <!-- Aquí realizo el filtrado con el condicional if -->
                     if ($persona[$penultimate_index] == "Amarillo") {
                         echo '<tr class="table-warning">';
                         foreach ($persona as $info) {
--- scripts/alert.js
+++ scripts/alert.js
@@ -1,8 +1,10 @@
 // Alert
+// Como por defecto se muestra el alert hay que ocultarlo
 $(document).ready(function () {
     $('#alertSuccess').hide()
 });
 
+// Cuando se presione el botón se mostrará el alert
 function showAlert() {
     $("#alertSuccess").show()
 
